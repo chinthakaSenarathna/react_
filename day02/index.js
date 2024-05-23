9a9f325330ff0befb12ba64096434f05d322ddec
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-24 01:37:36 +0530

    map() VS forEach() example code

--- day02/index.js
+++ day02/index.js
@@ -75,33 +75,63 @@
 
 // Example 01
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-function filterArray(arr, filterLogic) {
-    const filteredArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (filterLogic(arr[i])) {
-            filteredArr.push(arr[i]);
-        }
-    }
-    return filteredArr;
-}
+// function filterArray(arr, filterLogic) {
+//     const filteredArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (filterLogic(arr[i])) {
+//             filteredArr.push(arr[i]);
+//         }
+//     }
+//     return filteredArr;
+// }
 
-// Usage:
-const oddFilter = (num) => num % 2 !== 0;
-const evenFilter = (num) => num % 2 === 0;
+// // Usage:
+// const oddFilter = (num) => num % 2 !== 0;
+// const evenFilter = (num) => num % 2 === 0;
 
-console.log(filterArray(arr, oddFilter)); // [1, 3, 5, 7, 9, 11]
-console.log(filterArray(arr, evenFilter)); // [2, 4, 6, 8, 10]
+// console.log(filterArray(arr, oddFilter)); // [1, 3, 5, 7, 9, 11]
+// console.log(filterArray(arr, evenFilter)); // [2, 4, 6, 8, 10]
 
 
 
-// Example 02
+// // Example 02
 
-function addValue(value) {
-    return (input) => input + value;
-}
+// function addValue(value) {
+//     return (input) => input + value;
+// }
 
-const addFive = addValue(5);
-console.log(addFive(10)); // 15
-console.log(addFive(20)); // 25
+// const addFive = addValue(5);
+// console.log(addFive(10)); // 15
+// console.log(addFive(20)); // 25
+
+
+
+
+// -------------- map() VS forEach() --------------------------
+
+// map()
+
+const originalArray = [1, 2, 3, 4, 5];
+
+const squaredArray = originalArray.map(function(number) {
+    return number ** 2;
+});
+
+console.log(squaredArray); // [1, 4, 9, 16, 25]
+
+
+
+// forEach()
+
+const fruits = ['apple', 'banana', 'orange'];
+
+fruits.forEach(function(fruit) {
+    console.log(fruit);
+});
+
+// Output:
+// apple
+// banana
+// orange
