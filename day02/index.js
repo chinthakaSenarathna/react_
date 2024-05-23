97441a11fb779c8cf731c29d299132ce1a87e091
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-24 00:56:36 +0530

    What is the diferent -> var VS let VS const...

---
+++ day02/index.js
@@ -0,0 +1,69 @@
+// var x = 10;
+// var time = 50;
+// if (time > 10){
+//     var x=20;
+//     console.log(x);
+// }
+// console.log(x);
+
+
+// ------------------------------------------------
+
+// var y = 10;
+// console.log(y);
+
+// function abc(){
+//     var y = 20;
+//     console.log(y);
+// }
+
+// var y = 30;
+// console.log(y);
+
+// abc();
+
+// console.log(y);
+
+
+// --------------------------------------------------------
+
+
+// let x = 10;
+// let count = 50;
+
+// if(count > 10){
+//     let x = 20;
+//     console.log(x)
+// }
+
+// function abc(){
+//     let x = 30;
+//     console.log(x)
+// }
+
+// // let x = 40; -> given error...
+
+// console.log(x)
+
+// abc()
+
+
+
+// ---------------------------------------------------
+
+const x = 10;
+// x = 20; -> can't modify x
+
+if(true){
+    const x = 20;
+    console.log(x)
+}
+
+function abc(){
+    const x =30;
+    console.log(x)
+}
+
+console.log(x)
+
+abc()
