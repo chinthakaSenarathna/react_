b582f80b839a67ef57bbe8dc774e47e1af633d3d
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-25 13:17:54 +0530

    string, number, array -> find, but object, boolean -> we can't

--- start_/dojo-blog/src/App.jsx
+++ start_/dojo-blog/src/App.jsx
@@ -2,13 +2,23 @@ import './App.css'
 
 function App() {
   const title = "Welcom to the new blog";
+  const likes = 50;
+  const arr = ['x','y','z'];
+  const person = {
+    name: 'chinthaka',
+    age: 23
+  };
 
   return (
     <div className="App">
       <div className="Content">
         <h1 className="Title">App Component</h1>
         {/* inside the '{}' react know this is the variable */}
-        <p className="SubTitle">{ title }</p>
+        <h2 className="SubTitle">{ title }</h2>
+        {/* like data type is 'number' but, but react render that as string */}
+        <p className="Para01">Liked { likes } times</p>
+        <p className="Para02">{ arr }</p>
+        <p className="Para03">{ person }</p>
       </div>
     </div>
   )
