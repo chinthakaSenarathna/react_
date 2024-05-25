e040f411be602611be2d1638bb3cb2d586c37cc7
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-25 14:50:08 +0530

    without the anonymous function.... -> Event

--- start_/dojo-blog/src/components/Home.jsx
+++ start_/dojo-blog/src/components/Home.jsx
@@ -5,15 +5,21 @@ const Home = () => {
         console.log('hey');
     }
 
+    const handelClickEventAgain = (name) => {
+        console.log('hey '+name);
+    }
+
     return (
         <div className="home">
             <h2 className="home-title">HomePage</h2>
             {/* only set the reference */}
-            <button onClick={handelClickEvent} >Click Me</button>
+            <button onClick={handelClickEvent} >Click Me</button>                                                                           
 
-            
             {/* invoke the automatically, without using the onClick Event */}
             {/* <button onClick={handelClickEvent()} >Click Me</button> */}
+
+            {/* How to pass the arguments.... */}
+            <button onClick={handelClickEventAgain}>Click Me</button>  
         </div>
     )
 }
