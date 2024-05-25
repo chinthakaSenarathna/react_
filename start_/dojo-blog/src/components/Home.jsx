40ab3cc0bd5906b4851375ee62364b05b2b98bbb
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-25 14:42:08 +0530

    set the onClick Event...

--- start_/dojo-blog/src/components/Home.jsx
+++ start_/dojo-blog/src/components/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Home = () => {
+    const handelClickEvent = () => {
+        console.log('hey');
+    }
+
     return (
         <div className="home">
-            <h1 className="home-title">HomePage</h1>
+            <h2 className="home-title">HomePage</h2>
+            {/* only set the reference */}
+            <button onClick={handelClickEvent} >Click Me</button>
+
+            
+            {/* invoke the automatically, without using the onClick Event */}
+            {/* <button onClick={handelClickEvent()} >Click Me</button> */}
         </div>
     )
 }
