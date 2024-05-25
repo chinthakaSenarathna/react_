803b803cb212873e1cf4bd4b05a98c5a02617d21
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-25 14:29:26 +0530

    done style part

--- start_/dojo-blog/src/App.jsx
+++ start_/dojo-blog/src/App.jsx
@@ -1,34 +1,14 @@
-import './App.css'
+// import './App.css';
+import NavBar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
-  const title = "Welcom to the new blog";
-  const likes = 50;
-  const arr = ['x','y','z'];
-  const person = {
-    name: 'chinthaka',
-    age: 23
-  };
-
-  const URL = 'http://www.google.com';
 
   return (
     <div className="App">
-      <div className="Content">
-        <h1 className="Title">App Component</h1>
-        {/* inside the '{}' react know this is the variable */}
-        <h2 className="SubTitle">{ title }</h2>
-        {/* like data type is 'number' but, but react render that as string */}
-        <p className="Para0">Liked { likes } times</p>
-        <p className="Para0">{ arr }</p>
-        {/* Wrrong */}
-        {/* <p className="Para03">{ person }</p> */}
-
-        <p className="Para0">{ 10 }</p>
-        <p className="Para0">{ "Hello, Chinthaka" }</p>
-        <p className="Para0">{ [1,2,3,4,5] }</p>
-        <p className="Para0">{Math.random() * 100 }</p>
-
-        <a href={ URL }>Google Site</a>
+      <NavBar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   )
