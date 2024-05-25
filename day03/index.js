6f8fa248a29b89d60cf76e749ffda7d548d11125
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-25 08:00:25 +0530

    Virtual DOM vs Real DOM

--- day03/index.js
+++ day03/index.js
@@ -1,12 +1,15 @@
-var parentList =  document.getElementById('myList');
-// console.log(parentList);
+// Event Delegation....
 
-// Attach event handler to parent element
-parentList.addEventListener('click',handelItemClick)
+// var parentList =  document.getElementById('myList');
+// // console.log(parentList);
 
-// Event hanler function
-function handelItemClick(event){
-    // console.log(event);
-    var target = event.target;
-    console.log("Clicked:",target.textContent);
-}
+// // Attach event handler to parent element
+// parentList.addEventListener('click',handelItemClick)
+
+// // Event hanler function
+// function handelItemClick(event){
+//     // console.log(event);
+//     var target = event.target;
+//     console.log("Clicked:",target.textContent);
+// }
+
