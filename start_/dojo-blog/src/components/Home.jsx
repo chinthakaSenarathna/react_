8d45354632753cfd2b48ec6599d103aceef0d704
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-03 13:19:23 +0530

    Fetching Data inside the -> useEffect()

--- start_/dojo-blog/src/components/Home.jsx
+++ start_/dojo-blog/src/components/Home.jsx
@@ -2,13 +2,7 @@ import { useState, useEffect } from 'react'
 import BlogsList from './BlogsList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title:'My new website', body:'lorem ipsum...', author:'mario', id:1 },                                                                      
-        { title:'Welcome party!', body:'lorem ipsum...', author:'yoshi', id:2 },
-        { title:'Web dev top tips', body:'lorem ipsum...', author:'mario', id:3 }
-    ]);
-
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null);
 
     const deleteblog = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id != id);
@@ -17,17 +11,18 @@ const Home = () => {
 
     // useEffect with dependency array...
     useEffect(() => {
-        console.log("useEffect ran");
-        console.log(name);
-    },[name]);
+        fetch('http://localhost:8000/blogs')
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+            })
+    },[]);
 
     return (
         <div className="home">
-            <BlogsList blogs={ blogs } title={ 'All Blogs' } deleteblog={deleteblog} />
-            <BlogsList blogs={ blogs.filter((blog) => blog.author == 'mario') } title={ "Mario's Blogs" } deleteblog={deleteblog} />
-
-            <button onClick={() => setName('chinthaka')}>Change Name</button>
-            <p>{ name }</p>
+            { blogs && <BlogsList blogs={ blogs } title={ 'All Blogs' } deleteblog={deleteblog} /> }
         </div>
     )
 }
