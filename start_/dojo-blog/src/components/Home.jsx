374bf324836e7774a4dec70a8392539e4516bc5b
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-03 13:30:05 +0530

    option 01 -> map() read when initial value is null

--- start_/dojo-blog/src/components/Home.jsx
+++ start_/dojo-blog/src/components/Home.jsx
@@ -2,18 +2,18 @@ import { useState, useEffect } from 'react'
 import BlogsList from './BlogsList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+    const [blogs, setBlogs] = useState([]);
 
     const deleteblog = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id != id);
         setBlogs(newBlogs);
-    }
+    };
 
     const getBlogs = async() => {
         const res = await fetch('http://localhost:8000/blogs');
         const data = await res.json();
         setBlogs(data);
-    }
+    };
 
     // useEffect with dependency array...
     useEffect(() => {
@@ -22,7 +22,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            { blogs && <BlogsList blogs={ blogs } title={ 'All Blogs' } deleteblog={deleteblog} /> }
+            <BlogsList blogs={ blogs } title={ 'All Blogs' } deleteblog={deleteblog} />
         </div>
     )
 }
