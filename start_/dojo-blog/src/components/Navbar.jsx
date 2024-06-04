6bea3713b66db897de7fd66bda8ee1e3baf8e622
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-04 15:39:01 +0530

    <Link /> -> SPA...(Without reloading entaire page) Vs <a /> -> MPA...(Request to the Server / With reload entaire page)

--- start_/dojo-blog/src/components/Navbar.jsx
+++ start_/dojo-blog/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1 className="navbar-title">The Dojo Blog</h1>
             <div className="navbar-links">
-                <a href="/" className="navbar-links-link">Home</a>                                                  
-                <a href="/create" className="navbar-links-link">New Blog</a>
+                <Link to="/" className="navbar-links-link">Home</Link>                                                  
+                <Link to="/create" className="navbar-links-link">New Blog</Link>
             </div>
         </nav>
     )
