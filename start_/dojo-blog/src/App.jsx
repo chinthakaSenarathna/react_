6ced0fe49ab3d87abfc05678376b495a7023432a
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-04 17:15:37 +0530

    useEffect() cleanup function... -> setInterval(() => {},1000) & setTimeout(()=> {},1000)

--- start_/dojo-blog/src/App.jsx
+++ start_/dojo-blog/src/App.jsx
@@ -1,25 +1,33 @@
-// import './App.css';
+import { useState, useEffect } from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import NavBar from './components/Navbar';
-import Home from './components/Home';
-import TextInputWithFocusButton from './components/TextInputWithFocusButton';
-import Create from './components/Create';
-
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false);
 
   return (
-    <Router>
-      <NavBar />
-      <div className='content'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/create' element={<Create />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Deeecode</h1>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      { show && <Child /> }
+    </div>
   )
+}
+
+const Child = () => {
+  useEffect(() => {
+    let i = 0;
+    const getInterval = setInterval(() => {
+      console.log('hello - '+i);
+      i++;
+    },1000);
+
+    return () => {
+      clearInterval(getInterval);
+      console.log('hey, stop...');
+    }
+
+  },[])
+
+  return <h2>Child</h2>
 }
 
 export default App
